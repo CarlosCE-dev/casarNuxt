eb4b1b32f93a03742b6d40d3621b49fdafbd655a
CarlosCE-dev <c4r10z.n3t@gmail.com>
2020-08-04 19:19:51 -0600

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+     
+    <h3> Numero de intentos {{ intentos }}</h3>
+    <v-card class="card blue lighten-4" width="200px" height="286px" :style="`top:${ top };left:${ left}`" >
+        <v-card-text class="text-center black--text">
+          <v-avatar class="profile" color="grey" size="128" tile>
+            <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
+          </v-avatar>
+          <div class="pt-2">
+            Haz click para obligar a Carlos que se case contigo owo
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" class="btn" @click="hover" :ripple="false" block>
+            Boton
+          </v-btn>
+        </v-card-actions>
+    </v-card>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      intentos: 0,
+      top: '20px',
+      left: '30px'
+    }),
+    methods: {
+      hover() {
+
+          const h = document.documentElement.scrollHeight - 286;
+          const w = document.documentElement.scrollWidth - 200;
+          
+          
+          this.top = `${ Math.floor( Math.random() * h ) }px`;
+          this.left = `${ Math.floor( Math.random() * w ) }px`;
+          this.intentos++
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.card{
+  position:fixed;
+}
+.intentos{
+  position: absolute;
+};
+.full-height{
+  height: 100%;
+}
+.btn .v-btn::before{
+  background-color: transparent;
+}
+
+</style>
